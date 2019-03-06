6d4bb42c0f014d53d8d3f62a9157e6d8813979cb
Antonio Terreno <antonio.terreno@gmail.com>
2019-03-06 16:18:58 +0000

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,24 @@
 import React from "react"
+import { Link as GatsbyLink } from "gatsby"
 
-export default () => <div>Hello world!</div>
+function Homepage(props) {
+  const { search } = props.location
+  const link = `https://example.com/signup${search}`
+
+  return (
+    <div>
+      <ol>
+        <li>
+          <GatsbyLink to={link}>{link} Gatsby Link</GatsbyLink>
+        </li>
+        <li>
+          <a href={`${link}`}>{link} HTML HREF</a>
+        </li>
+        <li>
+          <button onClick={() => window.open(link)}>onClick button</button>
+        </li>
+      </ol>
+    </div>
+  )
+}
+export default Homepage
